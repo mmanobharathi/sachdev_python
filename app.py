fe0a1ecec9cd94fa6f3278919b5e55810f4b2c8b
jsachdev07 <48942130+jsachdev07@users.noreply.github.com>
2024-05-29 20:40:58 +0530

    fourth

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def showPinehead():
-    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Clear-Trail!</h1><h2>This is Version 2</h2><img src='https://media.licdn.com/dms/image/C510BAQGYGct9oxYKxA/company-logo_200_200/0/1630590592479/cleartrail_technologies_logo?e=2147483647&v=beta&t=nYF6pZLKxNBONLe5sJ7WSnQD7ollvMgQx6xZhrodPKM' width='40%' alt='Pinehead @ Clear-Trail'></div>"
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Clear-Trail!</h1><h2>This is Version 3</h2><img src='https://media.licdn.com/dms/image/C510BAQGYGct9oxYKxA/company-logo_200_200/0/1630590592479/cleartrail_technologies_logo?e=2147483647&v=beta&t=nYF6pZLKxNBONLe5sJ7WSnQD7ollvMgQx6xZhrodPKM' width='40%' alt='Pinehead @ Clear-Trail'></div>"
     return html
 
 if __name__ == "__main__":
